ebc755609de0297f42107c48c49f36e45ac1df93
Karel Vrabka <karel.vrabka@gmail.com>
2020-09-26 00:40:40 +0200

    Added function to display Sportka draws

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import tkinter as tk
 import datetime
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter.font import Font
+from bs4 import BeautifulSoup
+import requests
 
 
 
@@ -33,14 +35,15 @@ class Root(tk.Tk):
         self.my_font = Font(size=15)
         self.my_font_smaller = Font(size=11)
         self.iconbitmap('D:\\Python\\Projects\\fun_with_data\\cash_icon.ico')
-
+        self.radio_var = tk.StringVar()
+        self.radio_var.set('Kasička')
 
         # vykresleni rozhrani
         self.make_frames()
         self.make_labels()
         self.make_buttons()
         self.make_entries()
-
+        self.make_radiobuttons()
 
 
         self.bind('<Return>', lambda a: self.yes_click())
@@ -50,16 +53,21 @@ class Root(tk.Tk):
         # definice vsech framu v rozhrani
 
         self.frame_main = tk.LabelFrame(padx=5, pady=5)
-        self.frame_main.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky=tk.N)
+        self.frame_main.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky=tk.E + tk.W + tk.N)
 
         self.frame_error = tk.LabelFrame(padx=5, pady=5, text='Chybové hlášení')
-        self.frame_error.grid(row=1, column=0, rowspan=1, columnspan=3, padx=10, pady=10, sticky = tk.W + tk.E + tk.S)
+        self.frame_error.grid(row=2, column=0, rowspan=1, columnspan=3, padx=10, pady=10, sticky = tk.W + tk.E + tk.S)
 
         self.frame_graph = tk.LabelFrame(padx=1, pady=1, text='Sloupcový graf')
-        self.frame_graph.grid(row=0, column=3, rowspan=2, padx=10, pady=10)
+        self.frame_graph.grid(row=0, column=3, rowspan=3, padx=10, pady=10)
 
         self.frame_draws = tk.LabelFrame(padx=10, pady=10, text="Výsledky v daných kolech")
-        self.frame_draws.grid(row=2, column=0, columnspan=4, padx=10, pady=10, sticky = tk.W + tk.E)
+        self.frame_draws.grid(row=3, column=0, columnspan=4, padx=10, pady=10, sticky = tk.W + tk.E)
+
+        self.frame_radio = tk.LabelFrame(padx=5, pady=5, text='Výběr loterie')
+        self.frame_radio.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky=tk.E + tk.W + tk.N)
+
+
 
     def make_labels(self):
         # definice vsech labelu v rozhrani
@@ -73,7 +81,7 @@ class Root(tk.Tk):
         self.label_tyden = tk.Label(self.frame_main, text="Týden", bd=1, font=self.my_font)
         self.label_tyden.grid(row=0, column=1, columnspan=2, sticky=tk.N)
 
-        self.label_error = tk.Label(self.frame_error, text="", font=self.my_font)
+        self.label_error = tk.Label(self.frame_error, text="", font=self.my_font, fg="Red")
         self.label_error.grid(row=3, column=1, columnspan=3, rowspan=4, sticky=tk.N)
 
 
@@ -93,6 +101,13 @@ class Root(tk.Tk):
         self.entry_tyden = tk.Entry(self.frame_main, font=self.my_font, width=10)
         self.entry_tyden.grid(row=1, column=1, columnspan=2, padx=5)
         self.entry_tyden.insert(0, '1')
+
+    def make_radiobuttons(self):
+        possibilities = ["Kasička", "Sportka", "EuroMiliony"]
+        for p in possibilities:
+            tk.Radiobutton(self.frame_radio, variable=self.radio_var, text=p, value=p, font=self.my_font).pack(side=tk.LEFT)
+
+
 
     def next_click(self):
         # funkce tlacitka zvysujici hodnotu tydne
@@ -120,12 +135,11 @@ class Root(tk.Tk):
         if previous_week == 1:
             self.button_previous['state'] = 'disabled'
 
-    def yes_click(self):
-        # funkce pro zobrazeni grafu na zaklade precteni entry
+    def kasicka(self):
 
-        # nacteni linku
         try:
-            used_url = "http://www.sazka.cz/system/vyherka?year=2020&week={}&game=kasicka".format(self.entry_tyden.get())
+            used_url = "http://www.sazka.cz/system/vyherka?year=2020&week={}&game=kasicka".format(
+                self.entry_tyden.get())
             raw_data = pd.read_html(used_url)
 
             # vybrani potrebnych sloupcu a procisteni od NA
@@ -137,12 +151,11 @@ class Root(tk.Tk):
             for c in data.columns[1:]:
                 v_count = v_count.add(data[c].value_counts(), fill_value=0)
 
-
             self.draws = []
-            draw = [] # vytvoreni promenne, ktera nese datum a pote dve losovani z toho data
+            draw = []  # vytvoreni promenne, ktera nese datum a pote dve losovani z toho data
             for d in data.values:
                 if not draw:
-                    draw.append(d[0][:-5]) # pridani data
+                    draw.append(d[0][:-5])  # pridani data
 
                 time = d[0][-5:]
                 values = d[1:].astype(int).astype(str)
@@ -152,12 +165,10 @@ class Root(tk.Tk):
                     self.draws.append(draw)
                     draw = []
 
-
             self.print_draws()
 
-
             # vytvoreni figure
-            fig = plt.Figure(figsize=(10, 6),  dpi=100)
+            fig = plt.Figure(figsize=(10, 6), dpi=100)
             fig.suptitle("Čísla tažená v {}. týdnu".format(self.entry_tyden.get()))
 
             # vizualizace grafu
@@ -174,6 +185,69 @@ class Root(tk.Tk):
             self.print_error("TÝDEN ZADÁN VE ŠPATNÉM FORMÁTU")
         except ImportError:
             self.print_error("ŠPATNÝ FORMÁT TÝDNE")
+
+    def sportka(self):
+
+        try:
+
+            url = "https://www.sazka.cz/system/vyherka?year=2020&week={}&game=sportka".format(self.entry_tyden.get())
+            raw_data = pd.read_html(url)
+
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            table = soup.findAll("tr", {"class": "loscisla sans"})
+            date = soup.findAll("span", {"class": "spn s18b"})
+
+            for w in self.frame_draws.winfo_children():
+                w.destroy()
+
+            for w in self.frame_graph.winfo_children():
+                w.destroy()
+
+            for i in range(len(table)//3):
+                draw1 = table[3*i].text.strip().split()[2:]
+                draw2 = table[3*i+1].text.strip().split()[2:]
+                chance = table[3*i+2].text.strip().split()
+                single_draw_frame = tk.LabelFrame(self.frame_draws, padx=5, pady=5, text=date[i].text)
+
+                single_draw_frame.pack(side=tk.LEFT)
+
+                l1 = tk.Label(single_draw_frame, text="1.tah: " + ", ".join(draw1[:-1]) + ' |' + draw1[-1], font=self.my_font_smaller)
+                l2 = tk.Label(single_draw_frame, text="2.tah: " + ", ".join(draw2[:-1]) + ' |' + draw2[-1], font=self.my_font_smaller)
+                l3 = tk.Label(single_draw_frame, text="Šance: " + "".join(chance), font=self.my_font_smaller)
+
+                l1.grid(row=0, column=0, sticky=tk.W)
+                l2.grid(row=1, column=0, sticky=tk.W)
+                l3.grid(row=2, column=0, sticky=tk.W)
+
+
+
+
+            # vybrani potrebnych sloupcu
+
+        except IndexError:
+            self.print_error("TÝDEN NEEXISTUJE!")
+        except ValueError:
+            self.print_error("TÝDEN ZADÁN VE ŠPATNÉM FORMÁTU")
+        except ImportError:
+            self.print_error("ŠPATNÝ FORMÁT TÝDNE")
+
+
+    def eurojackpot(self):
+        pass
+
+    def yes_click(self):
+        # funkce pro zobrazeni grafu na zaklade precteni entry
+
+        # nacteni linku
+        if self.radio_var.get() == 'Kasička':
+            self.kasicka()
+        elif self.radio_var.get() == 'Sportka':
+            self.sportka()
+        else:
+            self.eurojackpot()
+
+
 
 
     def print_draws(self):
